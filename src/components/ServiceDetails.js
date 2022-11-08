29345806c086f31e5e3acc7772c3859c5cae1cc6
unknown <nurul.islam96@aol.com>
2022-11-09 02:45:29 +0600

    added review functionality

--- src/components/ServiceDetails.js
+++ src/components/ServiceDetails.js
@@ -1,22 +1,51 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
+import toast from "react-hot-toast";
 import { useLoaderData } from "react-router-dom";
 import reviewBanner from "../assets/review-banner.png";
 import { AuthContext } from "../contexts/AuthProvider";
 
 const ServiceDetails = () => {
-    const service = useLoaderData();
-    const { _id, name, price, description, thumbnail } = service;
-  const {user} = useContext(AuthContext)
+  const [reviews, setReviews] = useState();
+  const [refresh, setRefresh] = useState(false);
+  const service = useLoaderData();
+  const { user } = useContext(AuthContext);
+  const { _id, name, price, description, thumbnail } = service;
+
+  useEffect(() => {
+    fetch(`http://localhost:5000/reviews?serviceId=${_id}`)
+      .then((res) => res.json())
+      .then((data) => setReviews(data));
+  }, [_id, refresh]);
 
   const handleSubmitReview = (e) => {
-    e.preventDefault()
+    e.preventDefault();
     const form = e.target;
     const message = form.message.value;
     const username = user.displayName;
     const photoURL = user.photoURL;
     const email = user.email;
-    console.log(username, photoURL , email);
-}  
+    const serviceId = service._id;
+    fetch(`http://localhost:5000/reviews`, {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({
+        serviceId,
+        email,
+        username,
+        photoURL,
+        message,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.acknowledged) {
+          toast.success("Thanks for your Review");
+          setRefresh(!refresh);
+        }
+      });
+  };
 
   return (
     <section className="dark:bg-gray-800 dark:text-gray-100">
@@ -130,65 +159,65 @@ const ServiceDetails = () => {
                     </button>
                   </div>
                 </div>
-                <form 
-                onSubmit={handleSubmitReview}
-                className="flex flex-col w-full">
+                <form
+                  onSubmit={handleSubmitReview}
+                  className="flex flex-col w-full"
+                >
                   <input
-                  type='textarea'
-                  row="10"
+                    type="textarea"
+                    row="10"
                     placeholder="Message..."
                     className="p-4 rounded-md resize-none text-gray-800 bg-gray-200"
                     name="message"
+                    required
                   />
                   <input
                     className="py-4 my-8 font-semibold rounded-md cursor-pointer dark:text-gray-900 bg-violet-400"
-                    value={'Leave Feedback'}
+                    value={"Leave Feedback"}
                     type="submit"
                   />
                 </form>
               </div>
             </div>
             {/* Previous Review Section Start */}
-            <div className="container flex flex-col w-full max-w-lg p-6 mx-auto divide-y rounded-md divide-gray-700 bg-gray-800 text-gray-100">
-              <div className="flex justify-between p-4">
-                <div className="flex space-x-4">
-                  <div>
-                    <img
-                      src="https://source.unsplash.com/100x100/?portrait"
-                      alt=""
-                      className="object-cover w-12 h-12 rounded-full bg-gray-500"
-                    />
+            {reviews?.map((review) => (
+              <div key={review._id} review={review}>
+              <div className="container flex flex-col w-full max-w-lg p-6 mx-auto divide-y rounded-md divide-gray-700 bg-gray-800 text-gray-100">
+                <div className="flex justify-between p-4">
+                  <div className="flex space-x-4">
+                    <div>
+                      <img
+                        src={review.photoURL}
+                        alt=""
+                        className="object-cover w-12 h-12 rounded-full bg-gray-500"
+                      />
+                    </div>
+                    <div>
+                      <h4 className="font-bold">{review.username}</h4>
+                      <span className="text-xs text-gray-400">
+                        2 days ago
+                      </span>
+                    </div>
                   </div>
-                  <div>
-                    <h4 className="font-bold">Leroy Jenkins</h4>
-                    <span className="text-xs text-gray-400">2 days ago</span>
+                  <div className="flex items-center space-x-2 text-yellow-500">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 512 512"
+                      className="w-5 h-5 fill-current"
+                    >
+                      <path d="M494,198.671a40.536,40.536,0,0,0-32.174-27.592L345.917,152.242,292.185,47.828a40.7,40.7,0,0,0-72.37,0L166.083,152.242,50.176,171.079a40.7,40.7,0,0,0-22.364,68.827l82.7,83.368-17.9,116.055a40.672,40.672,0,0,0,58.548,42.538L256,428.977l104.843,52.89a40.69,40.69,0,0,0,58.548-42.538l-17.9-116.055,82.7-83.368A40.538,40.538,0,0,0,494,198.671Zm-32.53,18.7L367.4,312.2l20.364,132.01a8.671,8.671,0,0,1-12.509,9.088L256,393.136,136.744,453.3a8.671,8.671,0,0,1-12.509-9.088L144.6,312.2,50.531,217.37a8.7,8.7,0,0,1,4.778-14.706L187.15,181.238,248.269,62.471a8.694,8.694,0,0,1,15.462,0L324.85,181.238l131.841,21.426A8.7,8.7,0,0,1,461.469,217.37Z"></path>
+                    </svg>
+                    <span className="text-xl font-bold">4.5</span>
                   </div>
                 </div>
-                <div className="flex items-center space-x-2 text-yellow-500">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 512 512"
-                    className="w-5 h-5 fill-current"
-                  >
-                    <path d="M494,198.671a40.536,40.536,0,0,0-32.174-27.592L345.917,152.242,292.185,47.828a40.7,40.7,0,0,0-72.37,0L166.083,152.242,50.176,171.079a40.7,40.7,0,0,0-22.364,68.827l82.7,83.368-17.9,116.055a40.672,40.672,0,0,0,58.548,42.538L256,428.977l104.843,52.89a40.69,40.69,0,0,0,58.548-42.538l-17.9-116.055,82.7-83.368A40.538,40.538,0,0,0,494,198.671Zm-32.53,18.7L367.4,312.2l20.364,132.01a8.671,8.671,0,0,1-12.509,9.088L256,393.136,136.744,453.3a8.671,8.671,0,0,1-12.509-9.088L144.6,312.2,50.531,217.37a8.7,8.7,0,0,1,4.778-14.706L187.15,181.238,248.269,62.471a8.694,8.694,0,0,1,15.462,0L324.85,181.238l131.841,21.426A8.7,8.7,0,0,1,461.469,217.37Z"></path>
-                  </svg>
-                  <span className="text-xl font-bold">4.5</span>
+                <div className="p-4 space-y-2 text-sm text-gray-400">
+                  <p>
+                   {review.message}
+                  </p>
                 </div>
-              </div>
-              <div className="p-4 space-y-2 text-sm text-gray-400">
-                <p>
-                  Vivamus sit amet turpis leo. Praesent varius eleifend elit, eu
-                  dictum lectus consequat vitae. Etiam ut dolor id justo
-                  fringilla finibus.
-                </p>
-                <p>
-                  Donec eget ultricies diam, eu molestie arcu. Etiam nec lacus
-                  eu mauris cursus venenatis. Maecenas gravida urna vitae
-                  accumsan feugiat. Vestibulum commodo, ante sit urna purus
-                  rutrum sem.
-                </p>
               </div>
             </div>
+            ))}
             {/* Previous Review Section end */}
           </div>
           <div className="mt-10 lg:mt-0 lg:col-start-1 md:block hidden lg:row-start-1">
