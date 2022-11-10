23b1a9f28c2bd50945eb9a2b27e851f42e0dfed3
unknown <nurul.islam96@aol.com>
2022-11-10 21:50:50 +0600

    fixed get started button

--- src/components/Home.js
+++ src/components/Home.js
@@ -31,9 +31,9 @@ const Home = () => {
               Examine your{" "}
               <span className="text-blue-400 underline">Teeth</span>
             </h1>
-            <button className="w-full px-4 py-2 mt-4 text-xl font-semibold text-white uppercase transition-colors duration-300 transform bg-blue-600 rounded-md lg:w-auto hover:bg-blue-500 focus:outline-none focus:bg-blue-500">
+            <Link to={'/signup'} className="w-full px-4 py-2 mt-4 text-xl font-semibold text-white uppercase transition-colors duration-300 transform bg-blue-600 rounded-md lg:w-auto hover:bg-blue-500 focus:outline-none focus:bg-blue-500">
               Get Started
-            </button>
+            </Link>
           </div>
         </div>
       </div>
