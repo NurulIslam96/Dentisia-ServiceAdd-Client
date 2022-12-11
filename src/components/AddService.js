6dacbaa6adfa89d9a2165e2ba575a3b8300e135a
unknown <nurul.islam96@aol.com>
2022-12-11 23:09:55 +0600

    add service image fixed

--- src/components/AddService.js
+++ src/components/AddService.js
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import { Helmet } from "react-helmet-async";
 import toast from "react-hot-toast";
 import { AuthContext } from "../contexts/AuthProvider";
+import image from '../assets/addservice.gif'
 
 const AddService = () => {
   const { user } = useContext(AuthContext);
@@ -45,12 +46,8 @@ const AddService = () => {
       </Helmet>
       <div data-aos="fade-left" className="px-6 h-full text-gray-800">
         <div className="flex xl:justify-center lg:justify-between justify-center items-center flex-wrap h-full g-6">
-          <div className="grow-0 shrink-1 md:shrink-0 basis-auto xl:w-6/12 lg:w-6/12 md:w-9/12 mb-12 md:mb-0">
-            <img
-              src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
-              className="md:w-auto w-full"
-              alt=""
-            />
+          <div>
+            <iframe src="https://embed.lottiefiles.com/animation/106544" className="h-96 w-96"></iframe>
           </div>
           <div className="xl:ml-20 xl:w-5/12 lg:w-5/12 md:w-8/12 w-full mb-12 md:mb-0">
             <h1 className="text-3xl font-bold">
